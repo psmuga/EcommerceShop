be42783365562626fd041ebc07703b25af26ad66
piotr.smuga <smuga.piotr@hotmail.com>
2020-11-08 23:48:56 +0100

    Setup project, add react-bootstrap, add header, footer and HomeScreen

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+
+function App() {
+    return (
+        <>
+            <Header></Header>
+            <main className='py-3'>
+                <Container>
+                    <HomeScreen></HomeScreen>
+                </Container>
+            </main>
+            <Footer></Footer>
+        </>
+    );
+}
+
+export default App;
