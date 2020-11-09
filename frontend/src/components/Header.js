d139e0b60d4db16eb354597ea5472f957d2a01aa
piotr.smuga <smuga.piotr@hotmail.com>
2020-11-09 18:35:42 +0100

    Add routing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href='/'>Shop Brand</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Shop Brand</Navbar.Brand>
+                    </LinkContainer>
+
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                     <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='ml-auto'>
-                            <Nav.Link href='/cart'>
-                                <i className='fas fa-shopping-cart mr-2'></i>Cart
-                            </Nav.Link>
-                            <Nav.Link href='/login' className='ml-3'>
-                                <i className='fas fa-user mr-2'></i>Sign In
-                            </Nav.Link>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link>
+                                    <i className='fas fa-shopping-cart mr-2'></i>Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link className='ml-3'>
+                                    <i className='fas fa-user mr-2'></i>Sign In
+                                </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
