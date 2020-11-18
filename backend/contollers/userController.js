42e4f49245ec0189b879da2e5a34bd90061ba121
piotr.smuga <smuga.piotr@hotmail.com>
2020-11-18 18:59:41 +0100

    Generate jwt token

--- backend/contollers/userController.js
+++ backend/contollers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc        Auth user & get token
 // @route       POST /api/users/login
@@ -7,13 +8,14 @@ import User from '../models/userModel.js';
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
+
     if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null,
+            token: generateToken(user._id),
         });
     } else {
         res.status(401);
