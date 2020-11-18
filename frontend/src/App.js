39540d370d193861549df5c111eb19b4f727c6f1
piotr.smuga <smuga.piotr@hotmail.com>
2020-11-18 23:02:37 +0100

    Add login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LoginScreen from './screens/loginScreen';
 
 function App() {
     return (
@@ -13,9 +14,10 @@ function App() {
             <Header></Header>
             <main className='py-3'>
                 <Container>
-                    <Route path='/' component={HomeScreen} exact></Route>
+                    <Route path='/login' component={LoginScreen}></Route>
                     <Route path='/product/:id' component={ProductScreen}></Route>
                     <Route path='/cart/:id?' component={CartScreen}></Route>
+                    <Route path='/' component={HomeScreen} exact></Route>
                 </Container>
             </main>
             <Footer></Footer>
