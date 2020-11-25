dfc71eebdcff38f3987f030428e74c4ed2e828cc
piotr.smuga <smuga.piotr@hotmail.com>
2020-11-25 19:21:50 +0100

    Add endpoint for getting all users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../contollers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from '../contollers/userController.js';
+import { isAdmin, protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, isAdmin, getUsers);
 
 export default router;
