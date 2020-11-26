4371bb943db88354ac52733a305e266140d67961
piotr.smuga <smuga.piotr@hotmail.com>
2020-11-26 16:52:03 +0100

    Add possibility to delete users by admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from '../contollers/userController.js';
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser } from '../contollers/userController.js';
 import { isAdmin, protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 router.route('/').post(registerUser).get(protect, isAdmin, getUsers);
+router.route('/:id').delete(protect, isAdmin, deleteUser);
 
 export default router;
