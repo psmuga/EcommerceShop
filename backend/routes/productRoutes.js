aa7a455c00634bbaba1caf3f342b3747d9e95839
piotr.smuga <smuga.piotr@hotmail.com>
2020-11-28 12:12:57 +0100

    Add possibility to delete products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getProductById, getProducts } from '../contollers/productController.js';
+import { isAdmin, protect } from '../middleware/authMiddleware.js';
+import { getProductById, getProducts, deleteProduct } from '../contollers/productController.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).delete(protect, isAdmin, deleteProduct);
 
 export default router;
