33a67c639a54ccd089c685675d2c27fe2a78a336
piotr.smuga <smuga.piotr@hotmail.com>
2020-11-28 21:58:24 +0100

    Add product edit screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 function App() {
     return (
@@ -33,8 +34,9 @@ function App() {
                     <Route path='/product/:id' component={ProductScreen}></Route>
                     <Route path='/cart/:id?' component={CartScreen}></Route>
                     <Route path='/admin/userlist' component={UserListScreen}></Route>
-                    <Route path='/admin/productlist' component={ProductListScreen}></Route>
                     <Route path='/admin/user/:id/edit' component={UserEditScreen}></Route>
+                    <Route path='/admin/productlist' component={ProductListScreen}></Route>
+                    <Route path='/admin/product/:id/edit' component={ProductEditScreen}></Route>
                     <Route path='/' component={HomeScreen} exact></Route>
                 </Container>
             </main>
