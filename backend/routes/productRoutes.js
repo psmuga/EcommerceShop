e3ad3d4c82eb7839b65cf090ab390d40229c02ea
piotr.smuga <smuga.piotr@hotmail.com>
2020-11-28 16:18:18 +0100

    Enable to create and update product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { isAdmin, protect } from '../middleware/authMiddleware.js';
-import { getProductById, getProducts, deleteProduct } from '../contollers/productController.js';
+import { getProductById, getProducts, deleteProduct, updateProduct, createProduct } from '../contollers/productController.js';
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductById).delete(protect, isAdmin, deleteProduct);
+router.route('/').get(getProducts).post(protect, isAdmin, createProduct);
+router.route('/:id').get(getProductById).delete(protect, isAdmin, deleteProduct).put(protect, isAdmin, updateProduct);
 
 export default router;
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,7 +4,8 @@ import { Button, Table, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { listProducts, deleteProduct } from '../actions/productActions';
+import { listProducts, deleteProduct, createProduct } from '../actions/productActions';
+import { PRODUCT_CREATE_RESET } from '../constants/productConstatnts';
 
 const ProductListScreen = ({ history, match }) => {
     const dispatch = useDispatch();
@@ -15,16 +16,25 @@ const ProductListScreen = ({ history, match }) => {
     const productDelete = useSelector((state) => state.productDelete);
     const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete;
 
+    const productCreate = useSelector((state) => state.productCreate);
+    const { loading: loadingCreate, error: errorCreate, success: successCreate, product: createdProduct } = productCreate;
+
     const userLogin = useSelector((state) => state.userLogin);
     const { userInfo } = userLogin;
 
     useEffect(() => {
-        if (userInfo && userInfo.isAdmin) {
-            dispatch(listProducts());
-        } else {
+        dispatch({ type: PRODUCT_CREATE_RESET });
+
+        if (!userInfo.isAdmin) {
             history.push('/login');
         }
-    }, [dispatch, history, userInfo, successDelete]);
+
+        if (successCreate) {
+            history.push(`/admin/product/${createdProduct._id}/edit`);
+        } else {
+            dispatch(listProducts());
+        }
+    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct]);
 
     const deleteHandler = (id) => {
         if (window.confirm('Are you sure')) {
@@ -32,7 +42,9 @@ const ProductListScreen = ({ history, match }) => {
         }
     };
 
-    const createProductHandler = (product) => {};
+    const createProductHandler = () => {
+        dispatch(createProduct());
+    };
 
     return (
         <>
@@ -48,6 +60,8 @@ const ProductListScreen = ({ history, match }) => {
             </Row>
             {loadingDelete && <Loader></Loader>}
             {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+            {loadingCreate && <Loader></Loader>}
+            {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
             {loading ? (
                 <Loader></Loader>
             ) : error ? (
