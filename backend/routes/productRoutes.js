88cb4db1843ffb6dcdeecb8054bc3c29599b6754
piotr.smuga <smuga.piotr@hotmail.com>
2020-11-29 14:06:11 +0100

    Add backend logic for adding reviews

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import { isAdmin, protect } from '../middleware/authMiddleware.js';
-import { getProductById, getProducts, deleteProduct, updateProduct, createProduct } from '../contollers/productController.js';
+import {
+    getProductById,
+    getProducts,
+    deleteProduct,
+    updateProduct,
+    createProduct,
+    createProductReview,
+} from '../contollers/productController.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, isAdmin, createProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 router.route('/:id').get(getProductById).delete(protect, isAdmin, deleteProduct).put(protect, isAdmin, updateProduct);
 
 export default router;
