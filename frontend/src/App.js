d86ba226aff1d9af1f0fd9f31383fc06a88a1b61
piotr.smuga <smuga.piotr@hotmail.com>
2020-11-29 17:38:30 +0100

    Add search box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,6 +39,7 @@ function App() {
                     <Route path='/admin/productlist' component={ProductListScreen}></Route>
                     <Route path='/admin/product/:id/edit' component={ProductEditScreen}></Route>
                     <Route path='/admin/orderlist' component={OrderListScreen}></Route>
+                    <Route path='/search/:keyword' component={HomeScreen}></Route>
                     <Route path='/' component={HomeScreen} exact></Route>
                 </Container>
             </main>
